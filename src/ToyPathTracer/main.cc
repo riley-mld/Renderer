b2323067d1d55c33dee627467c7e9ecb88d7c2a9
riley-mld <41442181+riley-mld@users.noreply.github.com>
2025-05-31 15:45:43 +0100

    Camera

--- src/ToyPathTracer/main.cc
+++ src/ToyPathTracer/main.cc
@@ -1,70 +1,24 @@
 #include "utils.h"
 
+#include "camera.h"
 #include "hittable.h"
 #include "hittable_list.h"
 #include "sphere.h"
 
 #include <iostream>
 
-colour ray_colour(const ray& r, const hittable& world) {
-    hit_record rec;
-    if (world.hit(r, interval(0, infinity), rec)) {
-        return 0.5 * (rec.normal + colour(1, 1, 1));
-    }
-    vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-a) * colour(1.0, 1.0, 1.0) + a*colour(0.5, 0.7, 1.0);
-}
-
 int main()
 {
-    // Image
-    auto aspect_ratio = 16.0 / 9.0;
-    int image_width = 400;
-
-    // Caclualte image height and make sure it's at least 1
-    int image_height = int(image_width / aspect_ratio);
-    image_height = (image_height < 1)? 1 : image_height;
-
     // World
     hittable_list world;
 
     world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
     world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
-    // Camera
-    auto focal_lengrh = 1;
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (double(image_width)/image_height);
-    auto camera_center = point3(0, 0, 0);
+    camera cam;
 
-    // Calculate the vectors across the horizontal and down the vertical viewport edges
-    // Setting based on right handed coordianate system
-    auto viewport_u = vec3(viewport_width, 0, 0);
-    auto viewport_v = vec3(0, -viewport_height, 0);
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
 
-    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-    auto pixel_delta_u = viewport_u / image_width;
-    auto pixel_delta_v = viewport_v / image_height;
-
-    // Calculate the location of the upper left pixel.
-    auto viewport_upper_left = camera_center - vec3(0, 0, focal_lengrh) - viewport_u/2 - viewport_v/2;
-    auto pixel100_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-    // Renderer
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-    for (int j=0; j < image_height; j++) {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i=0; i < image_width; i++) {
-            auto pixel_center = pixel100_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-            auto ray_direction = pixel_center - camera_center;
-            ray r(camera_center, ray_direction);
-
-            colour pixel_colour = ray_colour(r, world);
-            write_colour(std::cout, pixel_colour);
-        }
-    }
-    
-    std::clog << "\rDone.           \n";
+    cam.render(world);
 }
