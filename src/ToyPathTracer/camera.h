321bf94eb6e3df410ba1371d2f324e15de71bd21
riley-mld <41442181+riley-mld@users.noreply.github.com>
2025-06-05 17:36:44 +0100

    Stratified sampling pixel rays

--- src/ToyPathTracer/camera.h
+++ src/ToyPathTracer/camera.h
@@ -29,9 +29,11 @@ class camera {
             std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
             for (int i=0; i < image_width; i++) {
                 colour pixel_colour(0, 0, 0);
-                for (int sample = 0; sample < samples_per_pixel; sample++) {
-                    ray r = get_ray(i, j);
-                    pixel_colour += ray_colour(r, max_depth, world);
+                for (int s_j = 0; s_j < sqrt_spp; s_j++) {
+                    for (int s_i = 0; s_i < sqrt_spp; s_i++) {
+                        ray r = get_ray(i, j, s_i, s_j);
+                        pixel_colour += ray_colour(r, max_depth, world);
+                    }
                 }
                 write_colour(std::cout, pixel_samples_scale * pixel_colour);
             }
@@ -44,6 +46,8 @@ class camera {
     private:
         int image_height;
         double pixel_samples_scale; // Colour scale factor for a sum of pixel samples
+        int sqrt_spp; // Square root of number of samples per pixel
+        double recip_sqrt_spp; // 1 / sqrt_spp
         point3 center; // Camera center
         point3 pixel100_loc; // Location of pixel 0, 0
         vec3 pixel_delta_u; // Distance to pixel to the right
@@ -57,7 +61,9 @@ class camera {
             image_height = int(image_width / aspect_ratio);
             image_height = (image_height < 1)? 1 : image_height;
 
-            pixel_samples_scale = 1.0 / samples_per_pixel;
+            sqrt_spp = int(std::sqrt(samples_per_pixel));
+            pixel_samples_scale = 1.0 / (sqrt_spp * sqrt_spp);
+            recip_sqrt_spp = 1.0 / sqrt_spp;
 
             center = lookfrom;
 
@@ -91,11 +97,11 @@ class camera {
             defocus_disk_v = v * defocus_radius;
         }
 
-        ray get_ray(int i, int j) const {
+        ray get_ray(int i, int j, int s_i, int s_j) const {
             // Construct a camera ray originating from the defocus disk and directed at a randomly
-            // sampled point around the pixel location i, j.
+            // sampled point around the pixel location i, j for stratified sample square s_i, s_j.
             
-            auto offset = sample_square();
+            auto offset = sample_square_stratified(s_i, s_j);
             auto pixel_sample = pixel100_loc + ((i + offset.x()) * pixel_delta_u) + ((j + offset.y()) * pixel_delta_v);
 
             auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
@@ -103,6 +109,15 @@ class camera {
             auto ray_time = random_double();
 
             return ray(ray_origin, ray_direction, ray_time);
+        }
+
+        vec3 sample_square_stratified(int s_i, int s_j) const {
+            // Returns the vector to a random point in the square sub-pixel specified by grid
+            // indices s_i and s_j, for an idealized unit square pixel [-.5,-.5] to [+.5,+.5].
+            auto px = ((s_i + random_double()) * recip_sqrt_spp) - 0.5;
+            auto py = ((s_j + random_double()) * recip_sqrt_spp) - 0.5;
+
+            return vec3(px, py, 0);
         }
 
         vec3 sample_square() const {
